ca2cdf7c363a9b9bb8250ebbacd6a6102d12653c
Loïc Griffié <lgriffie@appsido.com>
2013-11-21 20:22:24 +0100

    Remove duplicated code

--- src/AOTutorialViewController.m
+++ src/AOTutorialViewController.m
@@ -26,46 +26,6 @@
 #define dismissLabelColor   @"#000000"
 
 
-@interface UIColor (AOAdditions)
-
-/**
- * Generate UIColor with given Hexadecimal color such as 0x123456
- *
- * @param UInt32 The hexadecimal color
- * @return UIColor translation
- */
-
-+ (UIColor *)colorWithHex:(UInt32)col;
-
-/**
- * Generate UIColor with given Hexadecimal color string representationsuch as #123456
- *
- * @param NSString The hexadecimal color
- * @return UIColor translation
- */
-
-+ (UIColor *)colorWithHexString:(NSString *)str;
-
-@end
-
-@implementation UIColor (AOAdditions)
-
-+ (UIColor *)colorWithHexString:(NSString *)str {
-    const char *cStr = [str cStringUsingEncoding:NSASCIIStringEncoding];
-    long x = strtol(cStr+1, NULL, 16);
-    return [UIColor colorWithHex:(int)x];
-}
-
-+ (UIColor *)colorWithHex:(UInt32)col {
-    unsigned char r, g, b;
-    b = col & 0xFF;
-    g = (col >> 8) & 0xFF;
-    r = (col >> 16) & 0xFF;
-    return [UIColor colorWithRed:(float)r/255.0f green:(float)g/255.0f blue:(float)b/255.0f alpha:1];
-}
-
-@end
-
 @interface AOButton : UIButton
 
 @end
