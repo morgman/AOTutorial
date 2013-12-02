da02d06aaccf8150fee53fc9967886731805d633
Loïc Griffié <lgriffie@appsido.com>
2013-12-02 16:16:35 +0100

    Remove unwanted target

--- AOTutorialLib/AOTutorialLib.h
+++
@@ -1,9 +0,0 @@
-//
-//  AOTutorialLib.h
-//  AOTutorialLib
-//
-//  Created by Loïc GRIFFIE on 02/12/2013.
-//  Copyright (c) 2013 Appsido. All rights reserved.
-//
-
-#import "AOTutorialViewController.h"
